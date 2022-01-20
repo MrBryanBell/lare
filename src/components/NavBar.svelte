90d2f3ee2de2e5ae20922838613ad864e9c6a34f
Bryan Bell <bryanbellabc@gmail.com>
2022-01-20 05:27:11 -0600

    Añadí componente NavBar

---
+++ src/components/NavBar.svelte
@@ -0,0 +1,41 @@
+<script>
+    // your script goes here
+</script>
+
+
+
+
+<div>
+
+    <section>
+
+        <!-- LOGO -->
+        <a href="#">
+            <img src="/icons/lare-logo.svg" alt="">
+        </a>
+
+        
+        <!-- MENU DE NAVEGACIÓN -->
+        <nav>
+            <a href="#"> <img src="/icons/dashboard.svg" alt=""> </a>
+            <a href="#"> <img src="/icons/metrics.svg" alt=""> </a>
+            <a href="#"> <img src="/icons/ranking.svg" alt=""> </a>
+        </nav>
+
+    </section>
+
+</div>
+
+
+
+
+
+
+
+
+
+<style lang="scss">
+    @import './styles/Navbar.scss';
+
+
+</style>
