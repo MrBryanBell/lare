dccefa0b38b6d78efe11409cdc5615d42b168328
Bryan Bell <bryanbellabc@gmail.com>
2022-01-20 05:26:33 -0600

    Añadí componente Cycle Bar

---
+++ src/components/CycleBar.svelte
@@ -0,0 +1,47 @@
+<script>
+    // your script goes here
+    let data = {
+        carrera: 'c. de la Comunicación',
+        pensum: '2018 - 2021',
+        ciclo: 'octavo',
+    };
+
+    let { carrera, pensum, ciclo } = data;
+</script>
+
+
+
+<div class="main-container">
+
+    <!-- CYCLE-STATE-BAR -->
+
+    <div class="cycle-state-bar">
+        <img class="luckImage" src="/images/graphics.png" alt="">
+        <div>
+            <img src="/icons/cloud-sync.svg" alt="">
+            <p>CICLO /</p>
+            <h4>{ ciclo.toUpperCase() }</h4>
+        </div>
+
+        <section>
+            <img src="/icons/arrow-circle.svg" alt="">
+            <img src="/icons/arrow-circle.svg" alt="">
+        </section>
+    </div>
+
+    
+    <!-- INFO-BAR -->
+
+    <div class="cycle-info-bar">
+        <p>{carrera.toUpperCase()}</p>
+        <p>PENSUM {pensum}</p>
+    </div>
+
+</div>
+
+
+
+
+<style lang="scss">
+    @import './styles/_cycleBar.scss';
+</style>
