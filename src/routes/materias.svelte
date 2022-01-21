f6ef6b75b18c7c43ae67786f425231edce3751f5
Bryan Bell <bryanbellabc@gmail.com>
2022-01-21 00:57:58 -0600

    Esto realiza petición de las materias.

---
+++ src/routes/materias.svelte
@@ -0,0 +1,24 @@
+<script>
+    import { db } from '../firebase/firebaseConfig';
+    import { getDoc, doc, collection, addDoc } from 'firebase/firestore';
+    import { onMount } from 'svelte';
+    import { getData } from '../firebase/firebaseFunctions';
+
+    let materias = [ ];
+    
+    onMount(async () => {
+        materias = await getData('lareData', 'bryanbell');
+        console.log(materias);
+    });
+</script>
+
+<h1>Lista de Materias</h1>
+
+{#each materias as item}
+     <!-- content here -->
+     <p>{item.materia}</p>
+     <p>{item.nota}</p>
+{:else}
+     <p>No hay data aún</p>
+     <!-- empty list -->
+{/each}
