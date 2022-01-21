144ec822053b5f129a88921275370e00f598b4ac
Bryan Bell <bryanbellabc@gmail.com>
2022-01-21 00:52:16 -0600

    Añadí una función para solicitar data a Firestore.

---
+++ src/firebase/firebaseFunctions.js
@@ -0,0 +1,13 @@
+import { getDocs, getDoc, collection, doc } from "firebase/firestore";
+import { db } from "./firebaseConfig";
+
+async function getData(collectionID, documentID) {
+    const docRef = doc(db, collectionID, documentID);
+    const docSnap = await getDoc(docRef);
+    
+    return docSnap.data().ciclo1;
+}
+
+
+
+export { getData };
