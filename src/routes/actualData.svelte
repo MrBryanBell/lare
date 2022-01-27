7da615fe47fc56bea664c5e0fe8a92df8986d350
Bryan Bell <bryanbellabc@gmail.com>
2022-01-26 19:49:01 -0600

    Nueva Ruta: Muestra toda la información almacenada en los stores.

---
+++ src/routes/actualData.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { materias } from "../stores/materia_store";
+
+</script>
+
+<div>
+    {#each $materias as materia}
+         <!-- content here -->
+         <p>{materia.materia}  --- {materia.nota.toFixed(1)}</p>
+    {:else}
+         <!-- empty list -->
+         <p>No Data Now</p>
+    {/each}
+</div>
+
+<style>
+    div {
+        padding-left: 150px;
+    }
+</style>
