345932080b55593deec6abc4fca33424b9878217
Bryan Bell <bryanbellabc@gmail.com>
2022-01-26 19:35:53 -0600

    Limpieza de la ruta principal.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,16 +1,4 @@
-<script>
-    import { materias } from '../stores/materia_store.js'
+<script></script>
 
-</script>
 
-<h1>Materias</h1>
 
-{#each $materias as materia}
-     <!-- content here -->
-     <h4>{materia.materia}</h4>
-     <p>{materia.nota}</p>
-{:else}
-     <!-- empty list -->
-{/each}
-
-<a href="/materias">Materias</a>
