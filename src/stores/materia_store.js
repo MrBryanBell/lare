41ba41e3d4b2d13670acc3b6c0f18ba41804b5e5
Bryan Bell <bryanbellabc@gmail.com>
2022-01-26 19:37:34 -0600

    Modularize la data. Separé las UMG y el TotalUV.

--- src/stores/materia_store.js
+++ src/stores/materia_store.js
@@ -1,34 +1,57 @@
-import { writable, derived } from "svelte/store";
-import { getData } from "../firebase/firebaseFunctions";
+import { writable, derived } from 'svelte/store';
+import { getData } from '../firebase/firebaseFunctions';
 
 
-let materias = writable([]);
+// HACE LA PETICIÓN AL SERVIDOR Y HACE BIND DE LA DATA
+    let materias = writable([]);
 
-let promedio = derived(materias, ($materias) => {
-    let UMG = 
-        $materias
-        .map(({ nota, uv }) => nota * uv)
-        .reduce((acc, elem) => {
-            return acc + elem;
-    }, 0)
+    async function bindData() {
+        let data = await getData('lareData', 'bryanbell');
+        materias.set(data);
+    };
 
-    let TUV = 
-        $materias
-        .map(({ uv }) => uv)
-        .reduce((acc, elem) => {
-            return acc + elem;
-    }, 0)
-
-    return (UMG / TUV).toFixed(3);
-})
-
-async function bindData(){
-    let data = await getData('lareData', 'bryanbell');
-    materias.set(data);
-}
-
-bindData();
+    bindData();
 
 
 
-export { materias, promedio };
+// CALCULA EL TOTAL DE UNIDADES DE MÉRITO GANADAS
+    let totalUMG = derived(materias, 
+                        ($materias) => {
+                    	    let UMG = $materias
+                    	    	.map(({ nota, uv }) => nota * uv)
+                    	    	.reduce((acc, elem) => {
+                    	    		return acc + elem;
+                    	    	}, 0);
+                            
+                    	    return UMG;
+                        }
+                    );
+
+
+
+// CALCULA EL TOTAL DE UV
+    let totalUV = derived(materias, 
+                        ($materias) => {
+                    	    let TUV = $materias
+                    	    	.map(({ uv }) => uv)
+                    	    	.reduce((acc, elem) => {
+                    	    		return acc + elem;
+                    	    	}, 0);
+                            
+                            return TUV;
+                        }
+                    );
+
+
+
+// CALCULA EL PROMEDIO (DERIVADA DE DOS DERIVADAS)
+    let promedio = derived([totalUMG, totalUV], 
+                        ($array) => {
+                            const [ totalUMG, totalUV ] = $array;
+                            return (totalUMG / totalUV).toFixed(3);
+                        }
+                    );
+
+
+
+export { materias, promedio, totalUMG };
