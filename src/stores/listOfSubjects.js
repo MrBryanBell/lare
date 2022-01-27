c3e43881eb54ed89d1dcc0af5870eb18c5594c41
Bryan Bell <bryanbellabc@gmail.com>
2022-01-26 19:38:42 -0600

    Agregué una lista de las materias. Esto servirá para no almacenar uv y nombres de las materias en Firebase.

---
+++ src/stores/listOfSubjects.js
@@ -0,0 +1,80 @@
+let listOfSubjects = [
+    {
+        "code": "03GC01",
+        "materia": "análisis sociopolítico",
+        "uv": 3
+    },
+    {
+        "code": "03GC02",
+        "materia": "introducción a la comunicación",
+        "uv": 4
+    },
+    {
+        "code": "03GC03",
+        "materia": "fundamentos de la investigación",
+        "uv": 4
+    },
+    {
+        "code": "03GC04",
+        "materia": "gramática y redacción",
+        "uv": 4
+    },
+    {
+        "code": "03GC05",
+        "materia": "taller de expresión plástica",
+        "uv": 4
+    },
+    {
+        "code": "03GC06",
+        "materia": "sociología de la comunicación",
+        "uv": 3
+    },
+    {
+        "code": "03GC07",
+        "materia": "teoría de la comunicación",
+        "uv": 4
+    },
+    {
+        "code": "03GC08",
+        "materia": "investigación cuantitativa",
+        "uv": 4
+    },
+    {
+        "code": "03GC09",
+        "materia": "redacción periodística",
+        "uv": 4
+    },
+    {
+        "code": "03GC10",
+        "materia": "fundamentos de diseño gráfico y digital",
+        "uv": 4
+    },
+    {
+        "code": "03GC11",
+        "materia": "psicología de la comunicación y persuasión",
+        "uv": 3
+    },
+    {
+        "code": "03GC12",
+        "materia": "teoría administrativa",
+        "uv": 3
+    },
+    {
+        "code": "03GC13",
+        "materia": "investigación cualitativa",
+        "uv": 4
+    },
+    {
+        "code": "03GC14",
+        "materia": "periodismo digital",
+        "uv": 4
+    },
+    {
+        "code": "03GC15",
+        "materia": "semiótica de la imagen",
+        "uv": 3
+    }
+]
+
+
+export { listOfSubjects };
