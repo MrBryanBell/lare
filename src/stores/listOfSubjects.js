d1a8a12cd5a3df00a5cbae526a241abf9f4c0310
Bryan Bell <bryanbellabc@gmail.com>
2022-01-27 11:21:09 -0600

    Añadí nuevas materias.

--- src/stores/listOfSubjects.js
+++ src/stores/listOfSubjects.js
@@ -73,8 +73,167 @@ let listOfSubjects = [
         "code": "03GC15",
         "materia": "semiótica de la imagen",
         "uv": 3
+    },
+    {
+        "code": "03FC01",
+        "materia": "literatura aplicada a las comunicaciones",
+        "uv": 4
+    },
+    {
+        "code": "03FC02",
+        "materia": "mercadeo",
+        "uv": 4
+    },
+    {
+        "code": "03FC03",
+        "materia": "investigación aplicada a las comunicaciones",
+        "uv": 4
+    },
+    {
+        "code": "03FC04",
+        "materia": "diseño y producción impresa",
+        "uv": 4
+    },
+    {
+        "code": "03FC05",
+        "materia": "fotografía básica",
+        "uv": 4
+    },
+    {
+        "code": "03FC06",
+        "materia": "historia de las ideas políticas y comunicación",
+        "uv": 3
+    },
+    {
+        "code": "03FC07",
+        "materia": "relaciones públicas",
+        "uv": 3
+    },
+    {
+        "code": "03FC08",
+        "materia": "análisis de públicos y audiencias",
+        "uv": 3
+    },
+    {
+        "code": "03FC09",
+        "materia": "comunicación radiofónica",
+        "uv": 4
+    },
+    {
+        "code": "03FC10",
+        "materia": "fotografía avanzada",
+        "uv": 4
+    },
+    {
+        "code": "03FC11",
+        "materia": "legislación de la comunicación",
+        "uv": 4
+    },
+    {
+        "code": "03FC12",
+        "materia": "comunicación organizacional",
+        "uv": 3
+    },
+    {
+        "code": "03FC13",
+        "materia": "diseño y gestión de sitios web",
+        "uv": 4
+    },
+    {
+        "code": "03FC14",
+        "materia": "comunicación televisiva",
+        "uv": 4
+    },
+    {
+        "code": "03FC15",
+        "materia": "presupuesto y diseño de proyectos",
+        "uv": 4
+    },
+    {
+        "code": "03FC16",
+        "materia": "gerencia de empresas de medios de comunicación",
+        "uv": 4
+    },
+    {
+        "code": "03FC17",
+        "materia": "comunicación institucional",
+        "uv": 3
+    },
+    {
+        "code": "03FC18",
+        "materia": "comunicación y emprendimiento",
+        "uv": 4
+    },
+    {
+        "code": "03FC19",
+        "materia": "producción audiovisual",
+        "uv": 4
+    },
+    {
+        "code": "03FC20",
+        "materia": "introducción a la publicidad",
+        "uv": 4
+    },
+    {
+        "code": "03PCI0",
+        "materia": "proyecto cultural impreso",
+        "uv": 5
+    },
+    {
+        "code": "03TAM0",
+        "materia": "taller de animación",
+        "uv": 3
+    },
+    {
+        "code": "03CINO",
+        "materia": "comunicación intercultural",
+        "uv": 3
+    },
+    {
+        "code": "03CYE0",
+        "materia": "comunicación y educación",
+        "uv": 3
+    },
+    {
+        "code": "03PCA0",
+        "materia": "proyecto cultural audiovisual",
+        "uv": 5
+    },
+    {
+        "code": "03CGI0",
+        "materia": "comunicación, género e inclusión",
+        "uv": 3
+    },
+    {
+        "code": "03TGU0",
+        "materia": "taller de guiones",
+        "uv": 3
+    },
+    {
+        "code": "03IYS0",
+        "materia": "innovación y sostenibilidad",
+        "uv": 3
+    },
+    {
+        "code": "03PMC0",
+        "materia": "proyecto cultural multimedia",
+        "uv": 5
+    },
+    {
+        "code": "03LCI0",
+        "materia": "lenguaje cinematográfico",
+        "uv": 3
+    },
+    {
+        "code": "03ADM0",
+        "materia": "aplicaciones en dispositivos móviles",
+        "uv": 3
+    },
+    {
+        "code": "03SIC0",
+        "materia": "seminario de investigación en comunicación",
+        "uv": 3
     }
 ]
-
 
 export { listOfSubjects };
