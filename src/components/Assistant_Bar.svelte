fd866c1f4d438c607c8098766a26f0aae84a2ec8
Bryan Bell <bryanbellabc@gmail.com>
2022-01-26 19:46:19 -0600

    Nuevo Componente: Assistant Bar. Aún no es funcional.

---
+++ src/components/Assistant_Bar.svelte
@@ -0,0 +1,15 @@
+<script>
+    // your script goes here
+</script>
+
+<!-- markup (zero or more items) goes here -->
+
+<div></div>
+
+
+
+<style lang="scss" >
+    @import '../styles/assistant_bar.scss';
+</style>
+
+
