e26e68a5cef2a4ee495dc3f8e5d2ef17e9c7c390
Bryan Bell <bryanbellabc@gmail.com>
2022-01-26 19:47:38 -0600

    Nueva ruta creada para enviar data a firebase.

---
+++ src/routes/newComponent.svelte
@@ -0,0 +1,18 @@
+<script>
+    // your script goes here
+    import PopUp from "../components/PopUp.svelte";
+</script>
+
+<style>
+    /* your styles go here */
+    section {
+        padding-left: 200px;
+    }
+</style>
+
+<!-- markup (zero or more items) goes here -->
+
+<PopUp />
+<!-- <section></section> -->
+
+
