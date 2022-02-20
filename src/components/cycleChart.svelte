58376496d0e6dd6dffe8c0155191009d7d08aafd
Bryan Bell <bryanbellabc@gmail.com>
2022-02-20 04:24:48 -0600

    1. Personalizar el Tween Value (localValue), la transición es muy sosa por ahora. 2. Modularizar los estilos CSS (Valorarlo).

---
+++ src/components/cycleChart.svelte
@@ -0,0 +1,53 @@
+<script>
+    import { onMount } from "svelte";
+    import { tweened } from "svelte/motion";
+    import { barPerc } from '../stores/cycle_store';
+
+    let localValue = tweened(0);
+
+    onMount(() => {
+        localValue.set($barPerc)
+    });
+
+    $: $barPerc, localValue.set($barPerc)
+</script>
+
+
+
+<main>
+    <div>
+        <section
+            style:width={`${$localValue}%`}
+            >
+        </section>
+    </div>
+</main>
+
+
+
+<style lang="scss" >
+
+    main {
+        grid-area: lineBar;
+
+        height: 100%;
+        display: flex;
+        align-items: center;
+        z-index: 10;
+    }
+
+    div {
+        width: 100%;
+        height: 4px;
+        background-color: #F1F3F7;
+        border-radius: 4px;
+
+        overflow: hidden;
+
+        section {
+            height: 100%;
+            background: linear-gradient(90deg, #D7DCE9 0%, #BDC5DB 246.91%), #F1F3F7;;
+        }
+    }
+    
+</style>
