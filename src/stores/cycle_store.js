aa54ecb2bca8db958368f4f4cbc6618f302c443f
Bryan Bell <bryanbellabc@gmail.com>
2022-02-20 04:35:58 -0600

    Nueva Variable Derivada: barPerc. 1. Cambiar el nombre de estas variables por nombres mejor pensados y en inglés.

--- src/stores/cycle_store.js
+++ src/stores/cycle_store.js
@@ -1,14 +1,31 @@
-import { derived, writable } from "svelte/store";
+import { derived, writable } from 'svelte/store';
+import * as d3 from 'd3';
+import { scaleBand, scaleLinear, scaleSqrt } from 'd3-scale';
 
-let nombreCiclos = ['primero', 'segundo', 'tercero', 'cuarto', 'quinto', 'sexto', 'séptimo', 'octavo', 'noveno', 'décimo'];
+let nombreCiclos = [
+	'primero',
+	'segundo',
+	'tercero',
+	'cuarto',
+	'quinto',
+	'sexto',
+	'séptimo',
+	'octavo',
+	'noveno',
+	'décimo',
+];
+
 let cicloActual = writable(1);
 
-let cicloActualEnTexto = derived(cicloActual, 
-                            ($cicloActual) => {
-                            
-                                let index = $cicloActual;
-                                return nombreCiclos[index - 1];
-                            }
-                        );
+let cicloActualEnTexto = derived(cicloActual, ($cicloActual) => {
+	let index = $cicloActual;
+	return nombreCiclos[index - 1];
+});
 
-export { cicloActualEnTexto, cicloActual };
+let barPerc = derived(cicloActual, ($cicloActual) => {
+	const getPerc = d3.scaleLinear().domain([0, 10]).range([0, 100]);
+	let num = getPerc($cicloActual);
+	return num;
+});
+
+export { cicloActualEnTexto, cicloActual, barPerc };
