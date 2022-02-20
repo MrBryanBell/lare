a84e0a60f0e1a3655141dfc1ab91d187af22416e
Bryan Bell <bryanbellabc@gmail.com>
2022-02-20 04:36:10 -0600

    Just cleaning the code.

--- src/routes/newComponent.svelte
+++ src/routes/newComponent.svelte
@@ -1,18 +1,3 @@
-<script>
-    // your script goes here
-    import PopUp from "../components/PopUp.svelte";
-</script>
-
-<style>
-    /* your styles go here */
-    section {
-        padding-left: 200px;
-    }
-</style>
-
-<!-- markup (zero or more items) goes here -->
-
-<PopUp />
-<!-- <section></section> -->
+<script></script>
 
 
