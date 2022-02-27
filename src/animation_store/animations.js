bd0440cf5bd97dce5b569f200434d818ada1c4a4
Bryan Bell <bryanbellabc@gmail.com>
2022-02-27 06:36:41 -0600

    New Animations

--- src/animation_store/animations.js
+++ src/animation_store/animations.js
@@ -1,14 +1,17 @@
 import anime from 'animejs';
 import { currentTab } from '../stores/session_store';
+import { isAdderActive } from "../stores/session_store"
 import { get } from 'svelte/store';
 
-const getCoord = (x) => {
-    if (x === 1) return 0;
-    if (x === 2) return 52;
-    if (x === 3) return 104;
-};
+
 
 // NAV-BAR (ACTIVE-INDICATOR)
+    const getCoord = (x) => {
+        if (x === 1) return 0;
+        if (x === 2) return 52;
+        if (x === 3) return 104;
+    };
+
     function fade(node = undefined) {
         let num = get(currentTab);
         let coord = getCoord(num)
@@ -27,4 +30,33 @@ const getCoord = (x) => {
         })
     }
 
-export { fade };
+
+// ADDER APPEAR
+
+    function appear(node = undefined) {
+        anime({
+            targets: node,
+            translateX: ['480px', '72px'], // ['100%', '0%'],
+            duration: 400,
+            easing: 'cubicBezier(.1,.48,.22,.99)',
+        })
+
+        console.log('se ejecutó appear')
+    }   
+
+// ADDER DISSAPEAR
+
+    function disappear(node = undefined) {
+        anime({
+            targets: node,
+            translateX: '100%',
+            duration: 400,
+            easing: 'easeInQuad',
+        })
+
+        console.log('se ejecutó disappear')
+
+    }
+
+
+export { fade, appear, disappear };
