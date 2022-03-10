56e451937d1fdba70d52985a8dd3a17254f90420
Bryan Bell <bryanbellabc@gmail.com>
2022-03-10 17:44:24 -0600

    Función que borra materias

---
+++ src/algoritmos/deleteSubject.js
@@ -0,0 +1,28 @@
+import { materias } from '../stores/materia_store';
+import { listOfSubjects } from '../stores/listOfSubjects';
+
+
+function deleteSubject(id) {
+    let code;
+
+    materias.update( (materias) => {
+        let index = materias.findIndex( sub => sub.id === id)
+        let [ removedItem ] = materias.splice(index, 1)
+
+        //MENSAJES DE CONFIRMACIÓN
+        console.log('Se removió ' + removedItem.name);
+        console.table(materias);
+        code = removedItem.code;
+
+        return materias;
+    })
+
+    listOfSubjects.updateAdditions(code, -1);
+
+}
+
+
+
+export {
+    deleteSubject,
+}
