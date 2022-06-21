fc76244ed9147078da7f3b8856a82b3037a09c41
Bryan Bell <bryanbellabc@gmail.com>
2022-06-21 14:42:21 -0600

    📦 Install & configuration of Vitest, Testing Library, JestDom, jsDom.

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,18 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: preprocess(),
+  preprocess: preprocess(),
 	kit: {
 		adapter: adapter(),
-	}
+		vite: {
+			test: {
+				environment: 'jsdom', 
+				include: ['**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'], 
+				globals: true,
+				setupFiles: 'src/setupTests.ts',
+			},
+		},
+	},
 };
 
 export default config;
